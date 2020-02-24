ba72ba8bef7231e3c1b6c19ddcd3abb885402fe5
Patrick <patrick.hurley@codeimmersives.com>
2020-02-24 14:09:17 -0500

    finished

--- main.js
+++ main.js
@@ -1,10 +1,38 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+function addMeToEnd(arr){
+  arr.push('Colin');
+}
+function addMeToStart(arr){
+  arr.unshift('Colin');
+}
+function changeLast(arr, num){
+arr.pop();
+arr.push(num);
+}
 
+function changeAllValuesTo(arr,value){
+  for (let i = 0; i < arr.length; i++){
+arr[i] = value;
+    }
+  }
+function oddOrEven(arr){
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] % 2 === 0){
+    arr[i] = 'even';
+        } else {
+          arr[i]= 'odd';
+        }
+      }
+}
 
-
-
+function changeNextThreeToValue(start1,arr1,value1){
+  for (let i = 0; i < arr1.length; i++){
+    if (i >= start1 && i < start1 + 3){
+      arr1[i] = value1;}
+    }
+  }
 
 /*********************************
  * OUR CODE BELOW; DO NOT TOUCH! *
